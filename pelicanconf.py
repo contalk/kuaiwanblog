d95a680c227b80ca3f00b9b4f2d4beb71e102868
xiangcan <xiangcan@qvod.com>
2013-12-03 17:17:12 +0800

    增加向灿的博客

---
+++ pelicanconf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"快玩"
+SITENAME = u"快玩团队博客"
+SITEURL = 'http://blog.kuaiwan.com'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'zh'
+DISQUS_SITENAME = 'dntgss'
+
+# Blogroll
+LINKS =  ((u'快玩游戏', 'http://www.kuaiwan.com'),
+         )
+
+# Social widget
+SOCIAL = ((u'微博', 'http://weibo.com/kuaiwan'),
+          )
+
+DEFAULT_PAGINATION = 10
+
+PDF_GENERATOR = False 
+STATIC_PATHS = ["images"]
+
+
